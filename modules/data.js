e08783c2b71f421649329d8d001e50cc22f4becb
DiegoxK <diego.stv@outlook.es>
2024-06-21 17:20:38 -0500

    Export teams and quizes in data.js

--- modules/data.js
+++ modules/data.js
@@ -1,4 +1,28 @@
-let quizes = [
+export let teams = [
+  {
+    imagen: "gallinita.png",
+    nombre: "Equipo 1",
+    dinero: 0,
+    racha: 0,
+    ganando: false,
+  },
+  {
+    imagen: "gallinita.png",
+    nombre: "Equipo 2",
+    dinero: 0,
+    racha: 0,
+    ganando: false,
+  },
+  {
+    imagen: "gallinita.png",
+    nombre: "Equipo 3",
+    dinero: 0,
+    racha: 0,
+    ganando: false,
+  },
+];
+
+export let quizes = [
   {
     pregunta: "¿Cuándo inició la gran guerra?",
     respuestas: [
@@ -16,153 +40,6 @@ let quizes = [
       },
       {
         respuesta: "1914",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: true,
-      },
-    ],
-  },
-  {
-    pregunta: "",
-    respuestas: [
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
-        correcta: false,
-      },
-      {
-        respuesta: "",
         correcta: true,
       },
     ],
