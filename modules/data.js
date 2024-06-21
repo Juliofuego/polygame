6fc2af3c7524e509dc36b20d8b92ac81af4efccc
juliofuego <juliancuenta0@gmail.com>
2024-06-21 16:25:02 -0500

    Creada data.js

---
+++ modules/data.js
@@ -0,0 +1,989 @@
+let quizes = [
+  {
+    pregunta: "¿Cuándo inició la gran guerra?",
+    respuestas: [
+      {
+        respuesta: "1864",
+        correcta: false,
+      },
+      {
+        respuesta: "1945",
+        correcta: false,
+      },
+      {
+        respuesta: "1939",
+        correcta: false,
+      },
+      {
+        respuesta: "1914",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+  {
+    pregunta: "",
+    respuestas: [
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: false,
+      },
+      {
+        respuesta: "",
+        correcta: true,
+      },
+    ],
+  },
+];
