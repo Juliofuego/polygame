ef5b4d5931d0aedb4361f472d1a7b5f6172c8d91
DiegoxK <diego.stv@outlook.es>
2024-06-21 17:21:06 -0500

    Add modules for displaying teams and quizes

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-let money = 51;
-let boton = document.getElementById("boton");
-let suma = document.getElementById("suma");
-let pesos = document.getElementById("pesos");
-let billetos = document.getElementById("billetos");
+import { getQuizes } from "./modules/coin.js";
+import { getTeams } from "./modules/team-card.js";
 
-let rasgo = document.getElementById("rasgo");
-let novatu = document.getElementById("novatu");
-let darracha = document.getElementById("darracha");
-
-function cambiarasgo() {
-  rasgo.innerText = "Racha de 1";
-  rasgo.style.backgroundColor = "#B8A336";
-}
-darracha.addEventListener("click", cambiarasgo);
-
-console.dir(boton);
-function sumar() {
-  money = money + 5;
-  pesos.innerText = money;
-  billetos.style.backgroundColor = "#FCD015";
-}
-// function cambiazo() {
-//   document.getElementById("pesos").innerText = money;
-// }
-// boton.addEventListener("click", cambiazo);
-suma.addEventListener("click", sumar);
+getQuizes();
+getTeams();
