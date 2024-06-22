281e1064da87711dab41ccdde68ef02ca1dc0e6b
DiegoxK <diego.stv@outlook.es>
2024-06-21 19:46:47 -0500

    Updated team-card module to display crown for winning team

--- modules/team-card.js
+++ modules/team-card.js
@@ -1,14 +1,40 @@
 import { teams } from "./data.js";
 
+export function setCrown() {
+  let winningTeam = teams[0];
+
+  teams.forEach((currentTeam) => {
+    if (currentTeam.dinero > winningTeam.dinero) {
+      winningTeam = currentTeam;
+    }
+  });
+
+  winningTeam.ganando = true;
+}
+
 export function getTeams() {
   let teamContainer = document.getElementById("team-container");
 
-  teams.forEach((team, index) => {
+  teams.forEach((team) => {
     let teamCard = document.createElement("div");
     teamCard.className = "team-card";
 
     teamCard.innerHTML = `
+    ${
+      team.ganando
+        ? `
       <img
+        class="corona"
+        src="/public/corona.png"
+        alt="${team.name}"
+        height="60"
+        width="60"
+      />
+      `
+        : ""
+    }
+      <img
+        class="gallinita"
         src="/public/${team.imagen}"
         alt="${team.name}"
         height="90"
@@ -21,13 +47,14 @@ export function getTeams() {
         <div class="billetes" id="billetos">
           <p>Tiene $<span id="pesos">${team.dinero}</span></p>
         </div>
-        <div class="calidad" id="rasgo">
-          ${
-            team.racha === "novato"
-              ? '<p style="color: green;">Novato</p>'
-              : '<p style="color: red;">Experto</p>'
-          }
-        </div>
+        ${
+          team.racha === 0
+            ? '<div style="background: #638DEA;" class="calidad" id="rasgo"><p>Normal</p></div>'
+            : team.racha >= 1
+            ? `<div style="background: #B8A336;" class="calidad" id="rasgo"><p>Racha de ${team.racha}</p></div>`
+            : team.racha <= -1 &&
+              `<div style="background: #9D4949;" class="calidad" id="rasgo"><p>Perdedores</p></div>`
+        }
       </div>
     `;
     teamContainer.appendChild(teamCard);
