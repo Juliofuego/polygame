22a3dad16fd61e18473929c1d38839ed733061b8
DiegoxK <diego.stv@outlook.es>
2024-06-21 19:46:41 -0500

    Add shuffle function to utils module

---
+++ modules/utils.js
@@ -0,0 +1,19 @@
+export function shuffle(array) {
+  let shuffledArray = [...array];
+
+  let currentIndex = shuffledArray.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[currentIndex],
+    ];
+  }
+  return shuffledArray;
+}
