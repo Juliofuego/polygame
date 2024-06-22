6372af7cf28cd6a3eab4641fc95d98023006c5e3
DiegoxK <diego.stv@outlook.es>
2024-06-21 19:47:01 -0500

    Added timer to index.html and update script.js to display and update the timer

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 import { getQuizes } from "./modules/coin.js";
-import { getTeams } from "./modules/team-card.js";
+import { getTeams, setCrown } from "./modules/team-card.js";
 
+let fechaElement = document.getElementById("fecha");
+fechaElement.innerHTML = new Date().toLocaleDateString();
+
+let timer = document.getElementById("timer");
+let time = 0;
+
+setInterval(() => {
+  time++;
+  timer.innerHTML = `
+    ${Math.floor(time / 60)}:${time % 60 < 10 ? "0" : ""}${time % 60}
+  `;
+}, 1000);
+
+setCrown();
 getQuizes();
 getTeams();
